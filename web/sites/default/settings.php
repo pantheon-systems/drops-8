a7dfc9a5f425789c65e76d4d4e32789ae20116c1
Kevin Porras <kporras07@gmail.com>
2022-11-01 13:34:19 -0600

    Copy settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -9,21 +9,12 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * Include the Pantheon-specific settings file.
  *
  * n.b. The settings.pantheon.php file makes some changes
- *      that affect all environments that this site
+ *      that affect all envrionments that this site
  *      exists in.  Always include this file, even in
- *      a local development environment, to ensure that
+ *      a local development environment, to insure that
  *      the site settings remain consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
-
-/**
- * Skipping permissions hardening will make scaffolding
- * work better, but will also raise a warning when you
- * install Drupal.
- *
- * https://www.drupal.org/project/drupal/issues/3091285
- */
-// $settings['skip_permissions_hardening'] = TRUE;
 
 /**
  * If there is a local settings file, then include it
